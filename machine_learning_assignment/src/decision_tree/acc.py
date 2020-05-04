6ce6713b241e4475aaf867d6a10c17367facdf63
Molefe Molefe <1858893@students.wits.ac.za>
2020-05-04 16:38:37 +0200

    Finished Beta Version Of The Asssignment

---
+++ machine_learning_assignment/src/decision_tree/acc.py
@@ -0,0 +1,162 @@
+from train import *
+# from compare import *
+from statistics import mode
+from nested_lookup import nested_lookup
+from predict import *
+from visualise import *
+import seaborn as sn
+import matplotlib.pyplot as plt
+
+
+
+
+####################################################### Correct Methods #######################################################
+def convert_first(class_list):
+	class_dict = {"recurrence-events":1,"no-recurrence-events":0}
+	class_list = [class_dict[item] for item in class_list]
+	return class_list
+
+def convert(class_list):
+	class_unique = np.unique(class_list)
+	class_dict = {}
+	for index in range(len(class_unique)):
+		class_dict[class_unique[index]] = index
+
+	class_list =  [class_dict[item] for item in class_list]
+	return class_list
+
+
+
+#Accuracy
+#Assumes cost
+def accuracy(TP,TN,FP,FN):
+	numerator = TP + TN
+	denominator = TP + TN + FP + FN
+	return numerator/denominator
+	
+
+# When it actually says, i.e real data
+#how often does it predict yes
+def recall(TP,FN):
+  return TP/(TP+FN)
+
+
+#When it predicts yes , how often
+def precision(TP,FP):
+	return TP/(TP+FP)
+
+
+
+
+def f_measure(recall,precision):
+	numerator = 2* recall * precision
+	denominator = recall + precision
+	return numerator / denominator
+
+
+
+
+####################################################### Correct Methods #######################################################
+
+"""
+
+Example
+
+  y_act  y_pred
+0      0       1
+1      0       0
+2      0       0
+3      0       0
+4      0       0
+
+
+This implies that one of the columns has all 1s or all 0s
+
+Error Message {
+
+File "acc.py", line 121, in <module>
+    accuracy(test_data,model)
+  File "acc.py", line 73, in accuracy
+    TP = conf_matrix[1][1]
+}
+
+"""
+def prediction_accuracy(test_data,model):
+
+	# Collect all the predictions
+	prediction_list = predict_dataset(model,test_data)
+	convert_pred = convert(prediction_list)
+	# # Collect the actual values
+	actual = list(test_data.iloc[:,-1])
+	convert_act = convert(actual)
+
+	data = {"y_act":convert_act,"y_pred":convert_pred}
+	df = pd.DataFrame(data,columns = ["y_act","y_pred"])
+	conf_matrix = pd.crosstab(df["y_act"],df["y_pred"],rownames=["Actual"],colnames=["Predicted"],margins=True,margins_name="Total")
+	TP = conf_matrix[1][1]
+	TN = conf_matrix[0][0]
+	FP = conf_matrix[1][0]
+	FN = conf_matrix[0][1]
+	acc = accuracy(TP,TN,FP,FN)
+	recall_data = recall(TP,FN)
+	precision_data = precision(TP,FP)
+	f_measure_data = f_measure(recall_data,precision_data)
+	return [{"Accuracy":acc, "Recall":recall_data,"Precision":precision_data,"F Measure":f_measure_data},conf_matrix]
+
+
+
+
+
+def show_heatmap(conf_matrix):
+	sn.heatmap(conf_matrix,annot=True)
+	plt.title("Breast Cancer: Confusion Matrix")
+	plt.show()
+
+
+def draw(parent_name, child_name):
+    edge = pydot.Edge(parent_name, child_name)
+    graph.add_edge(edge)
+
+
+def visit(node, parent=None):
+    for k,v in node.items():
+        if isinstance(v, dict):      
+            if parent:
+                draw(parent, k)
+            visit(v, k)
+        else:
+            draw(parent, k)         
+            draw(k, k+'_'+v)
+
+
+org_features = ["Class","Age","Menopause","Tumor-Size","Inv-Node","Node-Caps","Deg-Malig","Breast","Breast-Quad","Irradiat"]
+features = ["Irradiat","Age","Menopause","Tumor-Size","Inv-Node","Node-Caps","Deg-Malig","Breast","Breast-Quad"]
+features_wc = ["Irradiat","Age","Menopause","Tumor-Size","Inv-Node","Node-Caps","Deg-Malig","Breast","Breast-Quad","Class"]
+dataset = pd.read_csv("breast-cancer.csv",sep=",",header=None,names=org_features)
+
+# Swap last column and first column - ID3 algorithm takes last column as class
+ndataset = dataset[features_wc]
+ndataset = ndataset.sample(frac=1)
+filter_dataset = ndataset.replace("?",np.NaN)
+
+
+# Remove all of the NaN values
+drop_filter_dataset = filter_dataset.dropna(how="any").reset_index(drop=True)
+
+
+#Split the data
+split = len(drop_filter_dataset) * 75//100
+train_data = drop_filter_dataset[:split]
+test_data = drop_filter_dataset[split:]
+
+model = ID3(train_data,train_data)
+
+
+
+#Visualise the model
+def decision_tree(test_data,model):
+	print("Accuracy")
+	print(prediction_accuracy(test_data,model)[0])
+	show_heatmap(prediction_accuracy(test_data,model)[1])
+
+decision_tree(test_data,model)
--- machine_learning_assignment/src/random_forest/train.py
+++ machine_learning_assignment/src/random_forest/train.py
@@ -1,6 +1,18 @@
-from statistics import mode
 import pandas as pd 
 import numpy as np
+
+
+ ################################################################################################################################3
+"""
+ TRAINING THE MODEL
+"""
+########################################### Correct Methods ###########################################
+def entropy(dataset):
+  unique_values =  list(dataset.iloc[:,-1].value_counts())
+  probs = [x/sum(unique_values) for x in unique_values]
+  entropy = sum([prob*np.log2(prob) for prob in probs])
+  return abs(entropy)
+
 
 def entropy_feature(dataset,column):
   unique_values = list(dataset[column].value_counts())
@@ -10,12 +22,40 @@ def entropy_feature(dataset,column):
   return abs(entropy)
 
 
+def info_gains(dataset):
+	cols = dataset.shape[1]
+	info_gains = {}
+	for col in range(cols-1):
+		unique_values = list(dataset.iloc[:,col].value_counts())
+		unique_index = dataset.iloc[:,col].value_counts().index.tolist()
 
-def entropy(dataset):
-  unique_values =  list(dataset.iloc[:,-1].value_counts())
-  probs = [x/sum(unique_values) for x in unique_values]
-  entropy = sum([prob*np.log2(prob) for prob in probs])
-  return abs(entropy)
+		info_gain = 0
+		data_entropy = entropy(dataset)
+		data_size = dataset.shape[0]
+
+		entropies = []
+
+		for unique_feature in unique_index:
+			filt = dataset.iloc[:,col] == unique_feature
+			feature_dataset = dataset[filt]
+			entropies.append((entropy(feature_dataset),feature_dataset.shape[0]))
+
+		branch_entropy = sum([ data[0] * data[1] for data in entropies])
+		info_gain = data_entropy - branch_entropy/data_size		
+
+	
+		info_gains[col] = info_gain
+	return info_gains
+
+
+def best_attribute(gains):
+	x  = []
+	for gain in gains.values():
+		x.append(gain)
+	m = max(x)
+	index = list(gains.keys())[list(gains.values()).index(m)]
+	return index
+
 
 def info_gain_feature(dataset,feature):
 	
@@ -49,48 +89,6 @@ def info_gains_features(dataset,features):
 	for feature,size in zip(features,range(feature_size)):
 		info_gains[feature] = info_gain_feature(dataset,feature)
 	return info_gains
-
-def info_gains(dataset):
-	cols = dataset.shape[1]
-	info_gains = {}
-	for col in range(cols-1):
-		unique_values = list(dataset.iloc[:,col].value_counts())
-		unique_index = dataset.iloc[:,col].value_counts().index.tolist()
-
-		info_gain = 0
-		data_entropy = entropy(dataset)
-		data_size = dataset.shape[0]
-
-		entropies = []
-
-		for unique_feature in unique_index:
-			filt = dataset.iloc[:,col] == unique_feature
-			feature_dataset = dataset[filt]
-			entropies.append((entropy(feature_dataset),feature_dataset.shape[0]))
-
-		branch_entropy = sum([ data[0] * data[1] for data in entropies])
-		info_gain = data_entropy - branch_entropy/data_size		
-
-	
-		info_gains[col] = info_gain
-	return info_gains
-
-def best_attribute(gains):
-	x  = []
-	for gain in gains.values():
-		x.append(gain)
-	m = max(x)
-	index = list(gains.keys())[list(gains.values()).index(m)]
-	return index
-
-def lazy_entropy(dataset):
-	entropy = abs(sum([(x/sum(list(dataset.iloc[:,-1].value_counts())))*np.log2(x/sum(list(dataset.iloc[:,-1].value_counts()))) for x in list(dataset.iloc[:,-1].value_counts())]))
-	return entropy
-
-def lazy_entropy(dataset,column):
-	entropy = abs(sum([(x/sum(list(dataset.iloc[:,column].value_counts())))*np.log2(x/sum(list(dataset.iloc[:,column].value_counts()))) for x in list(dataset.iloc[:,column].value_counts())]))
-	return entropy
-
  
 def ID3_features(sub_dataset,dataset,features,parent_node_target=None):
 	#If target values have the same value
@@ -130,49 +128,35 @@ def ID3(sub_dataset,dataset):
 	features = list(dataset.columns)
 	features.remove(features[-1])
 	return ID3_features(sub_dataset,dataset,features)
-# Get all the leaves of the tree
-# Will be used for bootstrao aggregration
+
+
 def get_leaves(tree,features=[]):
+
 	for k,v in tree.items():
 		if isinstance(v,dict):
 			get_leaves(v,features)
 		else:
 			# k is the ket
 			features.append(v)
+
 	return features
 
 
 #Which occurs most
-# But if they occur the same amount of times, then just pick the first one
 def get_mode(leaves):
-	unique_values = np.unique(leaves,return_counts=True)[1]
-	ans = True
-	value = unique_values[0]
-	for x in unique_values:
-		if x != value:
-			ans  = False 
-	if ans == True:
-		return leaves[0]
-	else:
-		return mode(leaves)
+	return mode(leaves)
 
 
-
-# dataset = pd.read_csv("tennis.csv",header=None,names=["Outlook","Temperature","Humidity","Windy","Play"])
-# features=["Outlook","Temperature","Humidity","Windy"]
-# split = len(dataset)* 50//100
-# train_data = dataset[:split]
-# test_data = dataset[split:]
-# # print(test_data)
-# model = ID3_features(dataset,dataset,features)
-# print(model)
+def decision_tree(train_data):
+	model = ID3(train_data,train_data)
+	return model
+	
+ 
+########################################### Correct Methods ###########################################
 
 
 
 
-
-
-
-
+################################################################################################################################3
 
 
